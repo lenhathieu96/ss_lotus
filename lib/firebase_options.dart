e8d06ceddf92892e532aa299396b7cad377c0e0a
HieuLe <lenhathieu96@gmail.com>
2025-02-01 18:14:08 +0700

    ci: check

--- lib/firebase_options.dart
+++ lib/firebase_options.dart
@@ -54,12 +54,12 @@ class DefaultFirebaseOptions {
   }
 
   static FirebaseOptions web = FirebaseOptions(
-    apiKey: dotenv.get('FIREBASE_API_KEY'),
-    appId: dotenv.get("FIREBASE_APP_ID"),
-    messagingSenderId: dotenv.get("FIREBASE_MESSAGING_SENDER_ID"),
-    projectId: dotenv.get("FIREBASE_PROJECT_ID"),
-    authDomain: dotenv.get("FIREBASE_AUTH_DOMAIN"),
-    storageBucket: dotenv.get("FIREBASE_STORAGE_BUCKET"),
-    measurementId: dotenv.get("FIREBASE_MEASUREMENT_ID"),
+    apiKey: dotenv.env['FIREBASE_API_KEY'] ?? "",
+    appId: dotenv.env['FIREBASE_APP_ID'] ?? "",
+    messagingSenderId: dotenv.env['FIREBASE_MESSAGING_SENDER_ID'] ?? "",
+    projectId: dotenv.env['FIREBASE_PROJECT_ID'] ?? "",
+    authDomain: dotenv.env['FIREBASE_AUTH_DOMAIN'] ?? "",
+    storageBucket: dotenv.env['FIREBASE_STORAGE_BUCKET'] ?? "",
+    measurementId: dotenv.env['FIREBASE_MEASUREMENT_ID'] ?? "",
   );
 }
